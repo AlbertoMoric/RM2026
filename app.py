632bc55238452d65f9147409bde28ba104bc9f2f
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-07-01 09:37:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 import streamlit as st
 
-# URL de la imagen de fondo (puedes usar una URL pública o un archivo local con base64)
-imagen_fondo_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/La_Liga_logo.svg/1200px-La_Liga_logo.svg.png"
+imagen_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/La_Liga_logo.svg/1200px-La_Liga_logo.svg.png"
 
-page_bg_img = f"""
-<style>
-.stApp {{
-  background-image: url("{imagen_fondo_url}");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-}}
-</style>
-"""
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.image(imagen_url, use_column_width=True)
 
 st.title("Análisis de Plantillas de Fútbol - LaLiga")
