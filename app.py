0064638fb8704f3474129872a59578e39e71cb1a
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-07-01 09:32:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Carga CSV desde archivo local en el mismo repositorio")
+st.title("Análisis de Plantillas de Fútbol con IA")
 
 # Ruta relativa al CSV
 ruta_csv = "data.csv"
