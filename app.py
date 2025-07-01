874fb3356452773c0c1a8cda317323767d6ef771
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-07-01 09:43:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ st.title("Análisis de Plantillas de Fútbol IA")
 equipos = {
     "Real Madrid": {
         "escudo": "https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg",
-        "csv": "real_madrid.csv"
+        #"csv": "real_madrid.csv"
     },
     "Barcelona": {
         "escudo": "https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg",
-        "csv": "barcelona.csv"
+        #"csv": "barcelona.csv"
     },
     "Atlético de Madrid": {
         "escudo": "https://upload.wikimedia.org/wikipedia/en/f/f4/Atletico_Madrid_2017_logo.svg",
-        "csv": "atletico.csv"
+        #"csv": "atletico.csv"
     }
 }
