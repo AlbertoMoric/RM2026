109153331623ab81add6e3b72ac6b0cd674bbd5f
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-07-01 10:48:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.title("Análisis de Plantillas de Fútbol IA")
+# URL del escudo del Real Madrid
+url_escudo_rm = "https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg"
+
+st.image(url_escudo_rm, width=150)
