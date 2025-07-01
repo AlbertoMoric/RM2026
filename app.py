3e7381b237be964399d671440bdceeaba75861ed
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-07-01 10:58:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st
-import pandas as pd
+
+st.title("Analista de Plantillas Inteligente")
+
+# URLs de los escudos
+escudos = {
+    "Real Madrid": "https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg",
+    "Barcelona": "https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg",
+    "Atlético de Madrid": "https://upload.wikimedia.org/wikipedia/en/f/f4/Atletico_Madrid_2017_logo.svg",
+}
+
+cols = st.columns(len(escudos))
+
+for i, (equipo, url) in enumerate(escudos.items()):
+    cols[i].image(url, width=100)
