3ad20b9304636bf8510fbe721b620e519c3377a4
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-07-01 09:38:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 imagen_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/La_Liga_logo.svg/1200px-La_Liga_logo.svg.png"
 
-st.image(imagen_url, use_column_width=True)
+st.image(imagen_url, use_column_width=False)
 
 st.title("Análisis de Plantillas de Fútbol - LaLiga")
